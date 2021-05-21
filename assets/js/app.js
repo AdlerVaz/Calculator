05dbde24f7a375517b2194604e5ec03c7f304ff0
krom81 <krom81@users.noreply.github.com>
2021-05-21 13:29:11 +0700

    add numkey func

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,15 @@
 (()=>{
+    let primaryDisplay = ["0","0"]
+    let secondaryDisplay = [""]
+    function updateDisplay(){
+        const ePD = document.getElementById('input')
+        const pD0 = Number(primaryDisplay[0])
+        const pD1 = primaryDisplay[1]
+        ePD.innerText = Number(pD1) ? `${pD0}.${pD1}`:`${pD0}`
+        ePD.value = Number(pD1) ? `${pD0}.${pD1}`:`${pD0}`
+    }
+    updateDisplay()
+
     function themeToggle(){
         const target = document.getElementById("themeToggle")
         target.addEventListener('mouseover',()=>{
@@ -18,4 +29,21 @@
         target.addEventListener('click',switchTheme)
     }
     themeToggle()
+    function numKeyClickAdd(i){
+        const jsnk = document.getElementsByClassName('jsnumkey')
+        const pd = primaryDisplay
+        for(i=0;i<jsnk.length;i++){
+            const t = jsnk[i]
+            t.addEventListener('click',()=>{
+                const x = t.innerText
+                if(true){
+                    pd[0] = `${pd[0]}${x}`
+                }else{
+                    pd[1] = `${pd[1]}${x}`
+                }
+                updateDisplay()
+            })
+        }
+    }
+    numKeyClickAdd()
 })()
