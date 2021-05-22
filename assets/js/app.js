ba8295aedc7c28f19ddb712a299df780b78e779c
krom81 <krom81@users.noreply.github.com>
2021-05-22 18:01:11 +0700

    make it work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,4 @@
 (()=>{
-    let primaryDisplay = ["0","0"]
-    let secondaryDisplay = [""]
-    function updateDisplay(){
-        const ePD = document.getElementById('input')
-        const pD0 = Number(primaryDisplay[0])
-        const pD1 = primaryDisplay[1]
-        ePD.innerText = Number(pD1) ? `${pD0}.${pD1}`:`${pD0}`
-        ePD.value = Number(pD1) ? `${pD0}.${pD1}`:`${pD0}`
-    }
-    updateDisplay()
-
-    function themeToggle(){
         const target = document.getElementById("themeToggle")
         target.addEventListener('mouseover',()=>{
 
@@ -27,23 +15,5 @@
             target.classList.add('bg-black')
         })
         target.addEventListener('click',switchTheme)
-    }
-    themeToggle()
-    function numKeyClickAdd(i){
-        const jsnk = document.getElementsByClassName('jsnumkey')
-        const pd = primaryDisplay
-        for(i=0;i<jsnk.length;i++){
-            const t = jsnk[i]
-            t.addEventListener('click',()=>{
-                const x = t.innerText
-                if(true){
-                    pd[0] = `${pd[0]}${x}`
-                }else{
-                    pd[1] = `${pd[1]}${x}`
-                }
-                updateDisplay()
-            })
-        }
-    }
-    numKeyClickAdd()
+    
 })()
